182f3f6b027cace00ef6a96dd315b05db446c1a6
Ivan Samardzic <isamar01@fesb.hr>
2021-03-13 01:56:22 +0100

    Change some design using CSS in JS

--- src/components/Advert/Advert.js
+++ src/components/Advert/Advert.js
@@ -3,7 +3,7 @@ import React from 'react'
 const Advert = ({image}) => {
     return (
         <div style={{display : 'flex', margin : '10px auto', cursor : 'pointer'}}>
-            <img src={image}  style={{width : '1220px', height : '450px',  margin : '20px auto'}}/>
+            <img src={image}  style={{width : '1200px', height : '450px',  margin : '0px auto'}}/>
         </div>
     )
 }
