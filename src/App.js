640f324d22e626d7dee2c24ece0bb4a65afbecaa
Ivan Samardzic <isamar01@fesb.hr>
2021-03-13 16:26:55 +0100

    Add Routes paths to App

--- src/App.js
+++ src/App.js
@@ -1,54 +1,28 @@
 import React from 'react'
-import {useState, useEffect} from 'react'
 import {BrowserRouter as Router, Switch, Route} from 'react-router-dom'
 import './App.css';
-import Header from './components/Header/Header'
-import Advert from './components/Advert/Advert'
-import RouteName from './components/RouteName/RouteName'
-import ArticlesBox from './components/ArticlesBox/ArticlesBox';
-import Spinner from './components/Spinner/Spinner'
-import Button from './components/Button/Button'
-import Footer from './components/Footer/Footer'
-import axios from 'axios';
+import Home from './routes/Home/Home'
+import Business from './routes/Business/Business'
+import Media from './routes/Media/Media'
+import Sports from './routes/Sports/Sports'
+import Science from './routes/Science/Science'
+import Tech from './routes/Tech/Tech'
+import Health from './routes/Health/Health'
 
 const App = () => {
-  const [headlineArticles, setHeadlineArticles] = useState({})
-  const [sideArticles, setSideArticles] = useState({})
-  const [loading, setLoading] = useState(true)
-  const [count, setCount] = useState(1)
-
-  useEffect(() => {
-        setLoading(true);
-        const searchArticles = async (count) => {
-          const responseSide = await axios.get(`https://newsapi.org/v2/top-headlines?country=ca&apiKey=4b39208e6f1d4eb1b50a674762d71892&pageSize=${count*4}`);
-          const responseSideJSON = await responseSide.data;
-
-          const responseHead = await axios.get(`https://newsapi.org/v2/top-headlines?country=us&apiKey=4b39208e6f1d4eb1b50a674762d71892&pageSize=${count*2}`);
-          const responseHeadJSON = await responseHead.data;
-
-          if(responseSideJSON && responseHeadJSON) {
-              console.log(loading)
-              setHeadlineArticles(responseHeadJSON)
-              setSideArticles(responseSideJSON)
-              setLoading(false)
-          }
-        };
-
-        searchArticles(count);
-        console.log(sideArticles)
-        console.log(loading)
-        console.log(count)
-  }, [count])
 
   return (
-    <div className='container-fluid'>
-      <Header size='36px' />
-      <Advert image='https://static.jutarnji.hr/images/live-multimedia/binary/2018/9/28/17/a1.png' />
-      <RouteName name='Home' />
-      {loading ? <Spinner /> : <ArticlesBox headlineArticles={headlineArticles} sideArticles={sideArticles} />}
-      <Button count={count} setCount={setCount} />
-      <Footer size='48px' />
-    </div>
+    <Router>
+      <Switch>
+        <Route exact path='/' component={Home} />
+        <Route exact path='/business' component={Business} />
+        <Route exact path='/media' component={Media} />
+        <Route exact path='/sports' component={Sports} />
+        <Route exact path='/science' component={Science} />
+        <Route exact path='/tech' component={Tech} />
+        <Route exact path='/health' component={Health} />
+      </Switch>
+    </Router>
   );
 }
 
