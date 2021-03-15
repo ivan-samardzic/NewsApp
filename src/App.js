ad0ca3b9bbca9322152a2e04b8a21c3e45ecb6c9
Ivan Samardzic <isamar01@fesb.hr>
2021-03-15 19:16:47 +0100

    Add NewsState Compo Bundle

--- src/App.js
+++ src/App.js
@@ -9,22 +9,25 @@ import Science from './routes/Science/Science'
 import Tech from './routes/Tech/Tech'
 import Health from './routes/Health/Health'
 import Filter from './routes/Filter/Filter'
+import NewsState from '../src/context/NewsState'
 
 const App = () => {
 
   return (
-    <Router>
-      <Switch>
-        <Route exact path='/' component={Home} />
-        <Route exact path='/business' component={Business} />
-        <Route exact path='/media' component={Media} />
-        <Route exact path='/sports' component={Sports} />
-        <Route exact path='/science' component={Science} />
-        <Route exact path='/tech' component={Tech} />
-        <Route exact path='/health' component={Health} />
-        <Route path='/:query' component={Filter} />
-      </Switch>
-    </Router>
+    <NewsState>
+      <Router>
+        <Switch>
+          <Route exact path='/' component={Home} />
+          <Route exact path='/business' component={Business} />
+          <Route exact path='/media' component={Media} />
+          <Route exact path='/sports' component={Sports} />
+          <Route exact path='/science' component={Science} />
+          <Route exact path='/tech' component={Tech} />
+          <Route exact path='/health' component={Health} />
+          <Route path='/:query' component={Filter} />
+        </Switch>
+      </Router>
+    </NewsState>
   );
 }
 
