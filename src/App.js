50bfa3e706ed14fc3671e01486f518ef495dfd73
Ivan Samardzic <isamar01@fesb.hr>
2021-03-16 21:09:44 +0100

    Read Me

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import {BrowserRouter as Router, Switch, Route} from 'react-router-dom'
-import './App.css';
+import './App.scss';
 import Home from './routes/Home/Home'
 import Business from './routes/Business/Business'
 import Media from './routes/Media/Media'
