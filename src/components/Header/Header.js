cfae993ad96c6fa7ccab08e6b0e9c59b24924b48
Ivan Samardzic <isamar01@fesb.hr>
2021-03-16 20:53:28 +0100

    Replace CSS with SCSS

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import './Header.css'
+import './Header.scss'
 import Logo from '../Logo/Logo'
 import Navbar from '../Navbar/Navbar'
 import SearchBox from '../SearchBox/SearchBox'
