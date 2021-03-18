d9037a9c9ed10095dd67bb5c647e04f52ec68757
Ivan Samardzic <isamar01@fesb.hr>
2021-03-18 22:24:01 +0100

    Change the dom inside compo

--- src/components/Advert/Advert.js
+++ src/components/Advert/Advert.js
@@ -1,9 +1,10 @@
 import React from 'react'
+import './Advert.scss'
 
 const Advert = ({image}) => {
     return (
-        <div style={{display : 'flex', margin : '10px auto', cursor : 'pointer'}}>
-            <img src={image}  style={{width : '1200px', height : '450px',  margin : '0px auto'}}/>
+        <div style={{display : 'flex', margin : '10px auto'}}>
+            <img src={image}  style={{width : '600px', height : '240px',  margin : '0px auto'}}/>
         </div>
     )
 }
