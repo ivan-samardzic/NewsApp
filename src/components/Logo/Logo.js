459dc47ea886b73f03623e497b8e749b76ada3ab
Ivan Samardzic <isamar01@fesb.hr>
2021-03-18 22:20:16 +0100

    Change the logo design

--- src/components/Logo/Logo.js
+++ src/components/Logo/Logo.js
@@ -6,8 +6,7 @@ const Logo = ({size}) => {
     return (
         <div className='logo-box'>
             <Link to='/'>
-            <i style={{fontSize : size}} className="fab fa-neos"></i><span style={{fontSize : size}}>ewsLetter</span>
-            {size === '48px' && <h3 className="logo-message">Where News Become Reality</h3>}
+            <i className="fab fa-neos"></i><span>ewsApp</span>
             </Link>
         </div>
     )
